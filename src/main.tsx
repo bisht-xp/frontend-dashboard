81a767410e997ff2f1b78529f2d415c88d820519
bisht-xp <bishtkamal2907@gmail.com>
2024-08-19 01:14:03 +0530

    updated the main.tsx file

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,27 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter } from "react-router-dom";
+import Layout from "./components/Layout.tsx";
+import { Provider } from "react-redux";
+import { store } from "./store/store.ts";
+import { Toaster } from "react-hot-toast";
 
-createRoot(document.getElementById('root')!).render(
+const queryClient = new QueryClient();
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Provider store={store}>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <Layout>
+            <Toaster />
+            <App />
+          </Layout>
+        </BrowserRouter>
+      </QueryClientProvider>
+    </Provider>
+  </StrictMode>
+);
